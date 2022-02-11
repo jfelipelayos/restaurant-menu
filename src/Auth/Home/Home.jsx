f1566ad6df9544b373b832ae49f546c20e3a42a6
Felipe Layos <jfelipelayos@gmail.com>
2022-02-11 17:39:14 -0500

    feat: Home component

---
+++ src/Auth/Home/Home.jsx
@@ -0,0 +1,13 @@
+/* eslint react/prop-types: 0 */
+import React from 'react'
+
+function Home ({ logout }) {
+  return (
+    <div className='container mt-5'>
+      <h1>Home</h1>
+    </div>
+
+  )
+}
+
+export default Home
